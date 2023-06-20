7a5482840b4d123702d928c477ca390ff34771ce
Azych1 <nagimov123@gmail.com>
2023-06-20 21:43:27 +0500

    задача решена

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    int a = 11;
+
+        System.out.println(squaringANumber(a));
+
+    }
+
+    public static int squaringANumber(int number) {
+        return (int) Math.pow(number, 2);
+    }
+}
